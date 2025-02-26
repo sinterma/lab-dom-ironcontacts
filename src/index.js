6cf9678393dc295ec7b38b9edbcf812a0d0be108
Sosha Fedotovskaia <sinterma@gmail.com>
2025-02-26 20:49:23 +0100

    done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-
 // ITERATION 0 | Example Row
 // Splice 1 element from the contacts array at the random index
 const randomIndex = Math.floor(Math.random() * contacts.length);
@@ -27,35 +26,51 @@ exampleRow.innerHTML = `
     </button>
   </td>
 `;
+const buttonElement = exampleRow.querySelector(".btn-delete");
+buttonElement.addEventListener("click", () => {
+  exampleRow.remove();
+
+});
+
+const likeButtonExample = exampleRow.querySelector(".btn-like");
+likeButtonExample.addEventListener("click", () => {
+  likeButtonExample.classList.toggle("selected");
+});
+
 
 tableBody.appendChild(exampleRow);
-
-
-
-
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
 
-// Your code goes here ...
+threeContacts.forEach((contact) => {
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>
+      <img src= "${contact.pictureUrl}" />
+    </td>
+    <td> ${contact.name} </td>
+    <td> ${contact.popularity.toFixed(2)} </td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>`;
 
+  const buttonElement = row.querySelector(".btn-delete");
+  buttonElement.addEventListener("click", () => {
+    row.remove();
 
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+    //
+  });
+  const likeButton = row.querySelector(".btn-like");
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("selected");
+  });
 
-  // ITERATION 3 - Like Buttons
-
-  // Your code goes here ...
-
-  
-  
-
-
-// Bonus: ITERATION 4 - Add Random Contacts
-
-// Your code goes here ...
+  tableBody.appendChild(row);
+});
